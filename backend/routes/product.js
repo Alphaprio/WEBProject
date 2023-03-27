647fba4d24634ab5ef28420f921f284ff9a32797
BEGUE MATHEO p2104835 <101584954+Alphaprio@users.noreply.github.com>
2023-03-27 17:45:19 +0200

    GET

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -6,25 +6,62 @@ import { getDetails } from '../validators/index.js'
 const router = express.Router()
 
 router.get('/api/products', async (req, res, next) => {
-  res.status(600).send()
+  /* const user = await User.findOne({
+    attributes: ['id', 'username'],
+    include: [{
+        model: Product,
+        as: 'product',
+        attributes: ['sellerID'],
+        where: { sellerId : user.id }
+     }]
+  }) */
+  res.json(await Product.findAll({ 
+    attributes: ['id', 'name', 'description', 'category','originalPrice', 'pictureUrl', 'endDate', 'sellerId']
+  } ))
 })
 
 router.get('/api/products/:productId', async (req, res) => {
-  res.status(600).send()
+  res.json(await Product.findAll({ 
+    attributes: ['id', 'name', 'description', 'category','originalPrice', 'pictureUrl', 'endDate', 'sellerId']
+  } ))
 })
 
 // You can use the authMiddleware with req.user.id to authenticate your endpoint ;)
 
-router.post('/api/products', (req, res) => {
-  res.status(600).send()
+router.post('/api/products', async (req, res) => {
+  if(req.user.id){
+    try {   
+      res.json(await Task.create(req.body))
+      
+    } catch (error) {
+      res.status(400).json({ error })
+      
+    }
+  }
 })
 
 router.put('/api/products/:productId', async (req, res) => {
-  res.status(600).send()
+  if(req.user.id){
+    try {   
+      res.json(await Task.create(req.body))
+      
+    } catch (error) {
+      res.status(400).json({ error })
+      
+    }
+  }
 })
 
 router.delete('/api/products/:productId', async (req, res) => {
-  res.status(600).send()
+  if(req.user.id){
+    try {   
+      res.json(await Task.create(req.body))
+      
+    } catch (error) {
+      res.status(400).json({ error })
+      
+    }
+  }
 })
 
 export default router
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,7 +4,9 @@ import { User, Product, Bid } from '../orm/index.js'
 const router = express.Router()
 
 router.get('/api/users/:userId', async (req, res) => {
-  res.status(600).send()
+  res.json(await Users.findAll({ 
+    attributes: ['id','username','email','password','admin']
+  } ))
 })
 
 export default router
